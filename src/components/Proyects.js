ab73e1ce76fe1250a0ddce38cf5c81a94682ad45
Nico <franckennicolas@gmail.com>
2023-09-30 14:04:13 -0300

    New Proyect

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -102,21 +102,19 @@ function Proyects() {
           </div>
         </div>
 
-        <div className="proyect-3 proyect-container">
+        <div className="proyect-5 proyect-container">
           <div className="left">
             <div className="top">
-              <h3 className="proyect-title">Songy &#127925;</h3>
+              <h3 className="proyect-title">Client's Landing Page &#128666;</h3>
               <p className="proyect-description">
-                An interactive, with a loading interface, search app. Using
-                Deezer's API, I am able to request songs data. You can even
-                listen to a preview!
+                A personalized landing page for a real client.
               </p>
             </div>
             <div className="bottom">
               <h3>Tech used</h3>
               <div className="proyect-skills">
                 <div className="skill-a"></div>
-                <div className="skill-c"></div>
+                <div className="skill-d"></div>
                 <div className="skill-g"></div>
               </div>
             </div>
@@ -124,22 +122,22 @@ function Proyects() {
           <div className="right">
             <a
               className="img-link"
-              href="https://songy-webapp.netlify.app/"
+              href="https://transportemultiservicios.netlify.app/"
               target="_blank"
               rel="noreferrer"
             >
-              <div className="img-proyect-3"></div>
+              <div className="img-proyect-5"></div>
             </a>
             <div className="proyect-links">
               <a
-                href="https://songy-webapp.netlify.app/"
+                href="https://transportemultiservicios.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="bx bx-sm bx-globe"></i>
               </a>
               <a
-                href="https://github.com/NicolasFrancken/Songy"
+                href="https://github.com/NicolasFrancken/transporte-multiservicios"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -195,6 +193,53 @@ function Proyects() {
                 <div className="skill-g"></div>
                 <div className="skill-h"></div>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="proyect-3 proyect-container">
+          <div className="left">
+            <div className="top">
+              <h3 className="proyect-title">Songy &#127925;</h3>
+              <p className="proyect-description">
+                An interactive, with a loading interface, search app. Using
+                Deezer's API, I am able to request songs data. You can even
+                listen to a preview!
+              </p>
+            </div>
+            <div className="bottom">
+              <h3>Tech used</h3>
+              <div className="proyect-skills">
+                <div className="skill-a"></div>
+                <div className="skill-c"></div>
+                <div className="skill-g"></div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <a
+              className="img-link"
+              href="https://songy-webapp.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="img-proyect-3"></div>
+            </a>
+            <div className="proyect-links">
+              <a
+                href="https://songy-webapp.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bx bx-sm bx-globe"></i>
+              </a>
+              <a
+                href="https://github.com/NicolasFrancken/Songy"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="bx bx-sm bxl-github"></i>
+              </a>
             </div>
           </div>
         </div>
